8a18ae30696ba3ce7f096d1fdd21c7ec458569ed
Arthur Candido <oarthurcandido@gmail.com>
2025-06-21 21:57:22 -0300

    :sparkles: feat: smooth scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import ModeToggle from "./ui/ModeToggle";
 import LangToggle from "./ui/LangToggle";
 import Link from "next/link";
 import NavigationMobileMenu from "./ui/NavigationMobileMenu";
+import { useSmoothScroll } from '@/hooks/useSmoothScroll';
 
 interface Dictionary {
   contact: string;
@@ -11,27 +12,35 @@ interface Dictionary {
 }
 
 function Header({ dictionary }: { dictionary: Dictionary }) {
+  const smoothScroll = useSmoothScroll();
   return (
     <header
-      className="max-w-2xl w-full m-auto h-fit  flex justify-between 
-      items-end snap-start  "
+      className="flex items-end justify-between w-full max-w-2xl m-auto h-fit snap-start "
     >
       <div className="">
         <h1 className="text-4xl font-[900] max-w-lg ml-2">Arthur</h1>
         <h2 className="text-4xl font-[900] max-w-lg ml-2">Candido</h2>
       </div>
-      <nav className="hidden sm:mx-4 sm:flex flex-wrap w-full justify-end mx-2 items-center text-lg">
-        <Link href="#projects" className="mr-2">
+      <nav className="flex-wrap items-center justify-end hidden w-full mx-2 text-lg sm:mx-4 sm:flex">
+        <Link 
+        href="#projects"
+        onClick={(e) => smoothScroll(e, 'projects')}
+        scroll={false} 
+        className="mr-2">
           {dictionary.projects}
         </Link>
         <span className="w-[1px] bg-black mr-2"></span>
-        <Link className="mr-2" href="#contact">
+        <Link 
+        className="mr-2"
+        onClick={(e) => smoothScroll(e, 'contact')}
+        scroll={false}  
+        href="#contact">
           {dictionary.contact}
         </Link>
         <LangToggle />
         <ModeToggle />
       </nav>
-      <div className="sm:hidden self-center mx-2 flex items-center justify-center align-middle">
+      <div className="flex items-center self-center justify-center mx-2 align-middle sm:hidden">
         <ModeToggle />
         <LangToggle />
         {/* <NavigationMobileMenu /> */}
